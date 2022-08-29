dc4702791c5d908c64e9f593387cc617f117a5dd
sandrorvr <sandrorvr@yahoo.com.br>
2022-08-29 00:15:22 -0300

    add topics

---
+++ blog/NewsView.py
@@ -0,0 +1,65 @@
+from dataclasses import replace
+import requests
+from bs4 import BeautifulSoup as btfs
+from random import choices
+
+
+class News:
+    def __init__(self, search):
+        self.search = search
+        self.url_base_google = f'https://www.google.com/search?hl=fr&q={self.search}&tbm=nws&tbs=sbd:1'
+        self.topics = [] # {'title': None, 'description': None, 'link': None}
+
+    def searchTAG(self, html, tag, att):
+        soup = btfs(html, features='html.parser')
+        topics = soup.findAll(tag, att)
+        return topics
+    
+    def formatHREF(self, href):
+        return href.split('&')[0][7:]
+
+    def formatDESC(self, desc):
+        return '. '.join(desc.split('.')[0:-1])[0:120] + '...'
+    
+    def formatTITLE(self, title):
+        return ' '.join(title.split(' ')[0:4])[0:25] + '...'
+
+    def getTopics(self):
+        req = requests.get(self.url_base_google).text
+        topics = self.searchTAG(req, 'div', {'class': 'Gx5Zad fP1Qef xpd EtOod pkphOe'})
+        return topics
+    
+    def fillTopicsNews(self):
+        for tp in self.getTopics():
+            href = tp.find('a')
+            title = tp.find('div', {'class': 'BNeawe vvjwJb AP7Wnd'})
+            descrition = tp.find('div', {'class': 'BNeawe s3v9rd AP7Wnd'})
+            date = tp.find('span', {'class': 'r0bn4c rQMQod'})
+            self.topics.append(
+                {'title': self.formatTITLE(title.text), 
+                'description': self.formatDESC(descrition.text), 
+                'link': self.formatHREF(href.attrs['href']), 
+                'date': date.text
+                })
+    
+    @staticmethod
+    def selectTopics(arrTopics, nTopics=4):
+        return choices(arrTopics, k=nTopics)
+        
+
+if __name__ == '__main__':
+    listTopics = []
+    buscas = ['Atlassian jira', 'asana', 'gestion', 'outils+de+gestion']
+    buscas = ['+'.join(i.split(' ')) for i in buscas]
+    for busca in buscas:
+        news = News(busca)
+        print(news.url_base_google)
+        news.fillTopicsNews()
+        tp = News.selectTopics(news.topics, 1)[0]
+        listTopics.append(tp)
+
+        print(tp['title'])
+        print(tp['description'])
+        print(tp['link'])
+        print(tp['date'])
+        print('========================')
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from .models import Profile, CoreSkills, OtherSkills, WorkExperience, WorkExperienceComplements, LatestWorks, About, Details, Tools, ToolsTopics
 from django.core.paginator import Paginator
 
+from .NewsView import News
+
 def formatExperience():
     RawExperience = WorkExperience.objects.all()
     size = len(RawExperience)
@@ -100,24 +102,15 @@ def detail(request, idWK):
 
 def news(request):
     profile = Profile.objects.get(id=1)
-    data = {
-        'profile': {
-            'first_name': profile.first_name,
-            'last_name': profile.last_name,
-            'birth': profile.birth,
-            'linkedin': profile.linkedin,
-            'gitHub': profile.gitHub
-        }
-    }
-    return render(request, 'news.html', data)
-
-
-def tools(request):
-    profile = Profile.objects.get(id=1)
-    tool = Tools.objects.all()
-    tool_pag = Paginator(tool, 1)
-    page_number = request.GET.get('page')
-    page_obj = tool_pag.get_page(page_number)
+    listTopics = []
+    buscas = ['Atlassian jira', 'asana', 'gestion', 'outils+de+gestion']
+    buscas = ['+'.join(i.split(' ')) for i in buscas]
+    for busca in buscas:
+        news = News(busca)
+        print(news.url_base_google)
+        news.fillTopicsNews()
+        tp = News.selectTopics(news.topics, 1)[0]
+        listTopics.append(tp)
 
     data = {
         'profile': {
@@ -127,6 +120,57 @@ def tools(request):
             'linkedin': profile.linkedin,
             'gitHub': profile.gitHub
         },
-        'page_obj': page_obj
+        'topics_line1': listTopics[0:2],
+        'topics_line2': listTopics[2:]
+    }
+    return render(request, 'news.html', data)
+
+
+def createTopics(topics, group=3):
+    arr = []
+    size_topics = len(topics)
+    start = [i for i in range(0, len(topics),group)]
+    if size_topics > group:
+        size_start = len(start) - 1
+        for i in range(size_start):
+            arr.append(topics[start[i]:start[i+1]])
+        
+        arr.append(topics[start[size_start-group]:])
+        return arr
+    else:
+        return arr.append(topics)
+
+def tools(request):
+    profile = Profile.objects.get(id=1)
+    tool = Tools.objects.all()
+    tool_pag = Paginator(tool, 1)
+    page_number = request.GET.get('page')
+    page_obj = tool_pag.get_page(page_number)
+
+    pag_now = int(page_number) if page_number != None else 1
+    topics = ToolsTopics.objects.filter(idToll=tool[pag_now-1].id).order_by('id')
+    arr = []
+    group = 3
+    size_topics = len(topics)
+    start = [i for i in range(0, len(topics),group)]
+    if size_topics > group:
+        size_start = len(start) - 1
+        for i in range(size_start):
+            arr.append(topics[start[i]:start[i+1]])
+        
+        arr.append(topics[start[size_start-group]:])
+    else:
+        arr.append(topics)
+
+    data = {
+        'profile': {
+            'first_name': profile.first_name,
+            'last_name': profile.last_name,
+            'birth': profile.birth,
+            'linkedin': profile.linkedin,
+            'gitHub': profile.gitHub
+        },
+        'page_obj': page_obj,
+        'topics': arr
     }
     return render(request, 'tools.html', data)
